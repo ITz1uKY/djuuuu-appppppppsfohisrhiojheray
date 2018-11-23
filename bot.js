65746fd2df56f607efb20495be527da952ac0f45
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2018-11-23 23:00:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,74 +1,44 @@
 const Discord = require('discord.js');
-const Canvas = require('canvas')
 const client = new Discord.Client();
-const prefix = '?';
- 
+var prefix = "1";
+
+
+
+
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` ?help | Dream System. ✨ `,"https://www.twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);  
 });
 
+client.on('ready', async() => {
+var server = "515615496910012418"; // ايدي السررفر
+var channel = "515616826537934863";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    },305);
+})
 
-
-ؤlient.on('message' , message => {
-  var prefix = "?";
-  if(message.author.bot) return;
-  if(message.content.startsWith(prefix + "send")) {
-    let args = message.content.split(" ").slice(1);
-
-
-    let suggestmessage = args.join(" ").slice(22);
-    let suggestchannel = message.mentions.channels.first();
-
-    if (!suggestchannel) {
-        return message.reply("Please Mention the channel!")
-    }
-
-    if (!suggestmessage) {
-        return message.reply("Plase Give Text To send Channel!")
-    
-         
-    }
-     message.delete();
-suggestchannel.send("@everyone  `||` @here ");
-    let embed = new Discord.RichEmbed()
-        .addField("**", `${suggestmessage}`)
-        .setFooter(`by ${message.author.tag}`)
-        .setTimestamp()
-    suggestchannel.send({
-        embed
-    }).then(msg => {
-        msg.react("✅").then(r => msg.react("❎"))
-    });
-
-
-    message.reply(`Your message is sended.`).then(msg => msg.delete(1000));
-    return;
-}
-});
 
 
  
 
-client.login('process.env.BOT_TOKEN');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+
+client.login(process.env.BOT_TOKEN)
